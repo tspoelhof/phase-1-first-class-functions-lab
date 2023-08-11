6015957f7391a488b63c94abdfe672942dba5def
Thomas Spoelhof <thomasjspoelhof@gmail.com>
2023-08-11 12:22:33 -0400

    a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo"'];
+
+function returnFirstTwoDrivers() {
+    return drivers.slice(0, 2);
+};
+
+function returnLastTwoDrivers(driversArray) {
+    return driversArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiply) {
+    return function (fareRate) {
+        return fareRate * multiply;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, drivers) {
+    return drivers (driversArray);
+}
